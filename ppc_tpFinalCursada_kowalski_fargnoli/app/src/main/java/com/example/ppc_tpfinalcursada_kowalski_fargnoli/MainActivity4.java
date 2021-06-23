64aad7a6e33b8fc17bda05e746c14404efbd2aaf
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2021-06-23 04:18:48 -0300

    Ksi termino esta wea

--- ppc_tpFinalCursada_kowalski_fargnoli/app/src/main/java/com/example/ppc_tpfinalcursada_kowalski_fargnoli/MainActivity4.java
+++ ppc_tpFinalCursada_kowalski_fargnoli/app/src/main/java/com/example/ppc_tpfinalcursada_kowalski_fargnoli/MainActivity4.java
@@ -4,55 +4,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity4 extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
-    Button calcular4;
+    RadioButton SI;
+    RadioButton NO;
+
+    Boolean buleano;
+
+    /* NO SIRVE
+    EditText paciente;
+    EditText riesgoRecurrente;
+    int riesgoRecurrenteInt;
+    EditText riesgoProgreso;
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-    };
 
+        RadioGroup SIoNO = findViewById(R.id.radioGroup);
+        SI = findViewById(R.id.si);
+        NO = findViewById(R.id.no);
+
+        SIoNO.setOnCheckedChangeListener(this);
+
+        Button atras = findViewById(R.id.atras4);
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        EditText editText = findViewById(R.id.riesgoRecurrente);
+        editText.setFilters(new InputFilter[]{ new InputFilterMinMax("1" , "10")});
+
+        EditText editText1 = findViewById(R.id.riesgoProgeso);
+        editText1.setFilters(new InputFilter[]{ new InputFilterMinMax("1" , "10")});
+
+        //NO SIRVE
         /*
-    // Agregar listener al botón
+        paciente = findViewById(R.id.editTextTextPersonName);
+        riesgoRecurrente = findViewById(R.id.riesgoRecurrente);
+        riesgoRecurrenteInt = Integer.valueOf(riesgoRecurrente.getText().toString());
 
-        https://parzibyte.me/blog/2019/02/25/validar-edittext-android-seterror-tooltip/
-    calcular4.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // Quitamos los errores
-            etNumero.setError(null);
-            String posibleNumero = etNumero.getText().toString();
-            // Vemos si está vacío...
-            // Notación yoda: https://parzibyte.me/blog/2018/09/24/notacion-yoda-en-programacion/
-            if ("".equals(posibleNumero)) {
-                // Primer error
-                etNumero.setError("Introduce un número");
-                // Le damos focus
-                etNumero.requestFocus();
-                // Y terminamos la ejecución
-                return;
-            }
-            // En caso de que hayan puesto algo, convertimos a entero
-            int numero = Integer.parseInt(posibleNumero);
-            // Comparar si está en el rango
-            if (numero >= 5 && numero <= 60) {
-                // La validación termina y hacemos lo que vayamos a hacer
-                Toast.makeText(MainActivity.this, "Todo correcto", Toast.LENGTH_SHORT).show();
-            } else {
-                // Si no, entonces indicamos el error y damos focus
-                etNumero.setError("Número fuera de rango");
-                etNumero.requestFocus();
-            }
-        }
-    })}; */
 
-    public void Atras (View view){
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        riesgoProgreso = findViewById(R.id.riesgoProgeso);
+        */
+
+
     }
+
+    public void onCheckedChanged(RadioGroup arg0, int arg1){
+        if (SI.isChecked()){
+            buleano = Boolean.TRUE;
+        }else{
+            buleano = Boolean.FALSE;
+        }
+    }
+
+
+    //NO SIRVE
+    /*
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("riesgoRecurrente", riesgoRecurrenteInt);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        riesgoRecurrenteInt = savedInstanceState.getInt("riesgoRecurrente");
+        riesgoRecurrente.setText(String.valueOf(riesgoRecurrenteInt));
+    }
+    */
 }
